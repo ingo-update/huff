0866b620ef254e3fa5da142bc988f9531b9022ac
ingo <devnull@localhost>
2018-10-09 02:38:49 +0200

    Add basic usage error checking

--- src/pak.c
+++ src/pak.c
@@ -1,5 +1,5 @@
 /* pak.c
- * Main function for the compression part of the huffman compression 
+ * Main function for the compression part of the huffman compression
  * programs.
  */
 
@@ -17,8 +17,20 @@ int main(int argc, char **argv)
   unsigned char inc;
   char outname[100];
 
+  /* Check that we got exactly one argument. */
+  if (2 != argc)
+    {
+      fprintf(stderr, "Usage:\n%s <filename>\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+
   /* Open the input file to create character statistics. */
   infile = fopen(argv[1] ,"r");
+  if (NULL == infile)
+    {
+      fprintf(stderr, "Could not open input file '%s'.\n", argv[1]);
+      exit(EXIT_FAILURE);
+    }
 
   /* Zero the histogram array. */
   for (i = 0 ; i < 256 ; ++i)
@@ -39,13 +51,24 @@ int main(int argc, char **argv)
   /* Close the infile. */
   fclose(infile);
 
-  /* Create output file name. (Could be smarter.) */
+  /* Create output file name. (TODO: Could be smarter.) */
   strncpy(outname, argv[1], 95);
   strncat(outname, SUFFIX, 95);
 
   /* Open the files for compression. */
   infile = fopen(argv[1], "r");
+  if (NULL == infile)
+    {
+      fprintf(stderr, "Could not open input file '%s'.\n", argv[1]);
+      exit(EXIT_FAILURE);
+    }
+
   outfile = fopen(outname, "w");
+  if (NULL == outfile)
+    {
+      fprintf(stderr, "Could not open output file '%s'.\n", outname);
+      exit(EXIT_FAILURE);
+    }
 
   /* Output the histogram. */
   hist_store(outfile, hist);
@@ -57,5 +80,5 @@ int main(int argc, char **argv)
   fclose(infile);
   fclose(outfile);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
